37996b20372bda77470aa282d691908c9eed4e7a
Oscar Nunez <noscar07@gmail.com>
2020-01-25 18:26:17 -0500

    added questions and answers

---
+++ code.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+      title: "The Efile Tower is Located in which country?",
+      choices: ["France", "Germany", "Spain", "Italy"],
+      answer: "France"
+    },
+    {
+      title: "Which Countries flag has red and yellow in it?",
+      choices: ["Italy", "Netherlands", "Spain", "England"],
+      answer: "Spain"
+    },
+    {
+        title: "In which Country can you find the Colosseum?",
+        choices: ["Ireland", "Netherlands", "Italy", "Greece"],
+        answer: "Italy"
+      },
+      {
+        title: "What Is the Captial of Germany?",
+        choices: ["Berlin", "Munich", "Hamburg", "Frankfurt"],
+        answer: "Berlin"
+      },
+      {
+        title: "Which Countries flag has blue and red in it?",
+        choices: ["Italy", "Greece", "Spain", "England"],
+        answer: "England"
+      }
+    
+  ];
